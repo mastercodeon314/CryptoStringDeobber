f89bc27f2523c0c4b388748736e56ff0b5c3d335
mastercodeon314 <78676320+mastercodeon314@users.noreply.github.com>
2021-08-14 02:08:44 -0500

    1

---
+++ dnlib-master/Examples/Program.cs
@@ -0,0 +1,14 @@
+namespace dnlib.Examples {
+	class Program {
+		static void Main(string[] args) {
+			// Just uncomment whatever you want to debug
+// 			Example1.Run();
+// 			Example2.Run();
+// 			Example3.Run();
+// 			Example4.Run();
+// 			Example5.Run();
+// 			Example6.Run();
+			Example7.Run();
+		}
+	}
+}
